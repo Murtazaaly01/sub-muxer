82ae4865932ca3b3db0c31878e68f53bf1cc05d5
Murtazaaly01 <86062433+Murtazaaly01@users.noreply.github.com>
2021-07-02 23:22:20 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get('BOT_TOKEN',None)
-    APP_ID = os.environ.get('APP_ID',None)
-    API_HASH = os.environ.get('API_HASH',None)
+    BOT_TOKEN = os.environ.get('BOT_TOKEN',1745313525:AAHEjKFI6PYKV1f5bg7HgzpXT4mMfPqaRkg)
+    APP_ID = os.environ.get('APP_ID',2361573)
+    API_HASH = os.environ.get('API_HASH',116203ba585711bffe7c2692c82cd3e2)
 
     #comma seperated user id of users who are allowed to use
-    ALLOWED_USERS = [x.strip(' ') for x in os.environ.get('ALLOWED_USERS','1098504493').split(',')]
+    ALLOWED_USERS = [x.strip(' ') for x in os.environ.get('1629499623','1098504493').split(',')]
 
     DOWNLOAD_DIR = 'downloads'
